3db25e3e736f26ae38c1cebd90f28142ac7e0cad
Kononov Arseniy <kononovarseniy@gmail.com>
2016-05-18 15:13:17 +0700

    S-expression parser created.

--- SParser/Program.cs
+++ SParser/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LexicalAnalyzer;
+using System.IO;
 
 namespace SParser
 {
@@ -12,37 +14,101 @@ namespace SParser
         class MyToken : Token
         {
             public string Text;
-            public MyToken(Token token, string text) : base(token)
+            public MyToken(Token token, string sourceString) : base(token)
             {
-                Text = text;
+                Text = sourceString.Substring(token.Start, token.Length);
+            }
+            public override string ToString() => $"\"{Text}\"";
+        }
+        class StrToken : MyToken
+        {
+            public string Value = "";
+            public StrToken(Token token, string sourceString) : base(token, sourceString) {
+                Value = Text.Substring(1, token.Length - 2).Replace("\"\"", "\"");
+            }
+            public override string ToString() => $"\"{Text}\" value=\"{Value}\"";
+        }
+        static bool TryGetCurrent<T>(IEnumerator<T> en, out T result) where T : class
+        {
+            result = null;
+            try
+            {
+                result = en.Current;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+            return result != null;
+        }
+        static IEnumerable<SExpr> ParseSExprImpl(IEnumerator<MyToken> input, bool isTopLevel = true)
+        {
+            while (true)
+            {
+                if (!input.MoveNext())
+                {
+                    if (isTopLevel) yield break;
+                    else throw new ArgumentException();
+                }
+                MyToken current = input.Current;
+                if (!isTopLevel && current.Class == "closing-bracket")
+                    yield break;
+                else if (current.Class == "atom")
+                    yield return new SAtom(current.Text);
+                else if (current.Class == "str-atom")
+                    yield return new SString(((StrToken)current).Value);
+                else if (current.Class == "opening-bracket")
+                    yield return new SList(ParseSExprImpl(input, false));
             }
         }
+        static IEnumerable<SExpr> ParseSExpr(IEnumerable<MyToken> input)
+        {
+            var en = input.GetEnumerator();
+            return ParseSExprImpl(en);
+        }
+
         static void Main(string[] args)
         {
             Fsm lexer = new Fsm();
 
             FsmState space = new FsmState("space");
             space.Add(@"\s", space);
+
             FsmState atom = new FsmState("atom");
-            atom.Add(@"[^\s()]", atom);
+            atom.Add(@"[^\s()""]", atom);
+
+            FsmState str = new FsmState("str-atom");
+            str.Add(@"[^""]", str);
+            str.Add(@"""", new FsmState() { { @"""", str } });
             FsmState first = new FsmState() {
-                                 { @"(", new FsmState("opening bracket") },
-                                 { @")", new FsmState("closing bracket") },
-                                 { @"[^\s()]", atom },
-                                 { @"\s", space }
+                                 { @"(", new FsmState("opening-bracket") },
+                                 { @")", new FsmState("closing-bracket") },
+                                 { @"[^\s()""]", atom },
+                                 { @"\s", space },
+                                 { @"""", str }
                              };
             lexer.FirstState = first;
-            string input = "(asd   zxc(asd ((asd)) (()  )))";
+            string input = File.ReadAllText(@"..\..\..\input.txt");
             var output = Fsm.GetTokens(lexer.GetLexemes(input), (lex) =>
             {
-                Token res = new MyToken(lex,
-                    input.Substring(lex.Start, lex.Length));
-                return new List<Token>() { res };
+                Token res;
+                if (lex.Class == null)
+                    throw new Exception();
+                if (lex.Class == "str-atom")
+                    res = new StrToken(lex, input);
+                else
+                    res = new MyToken(lex, input);
+                var list = new List<Token>();
+                if (lex.Class != "space")
+                    list.Add(res);
+                return list;
             });
-            foreach (var tok in output)
+            /*foreach (var tok in output)
             {
-                Console.WriteLine($"{tok.Class ?? "ERROR",-20} = \"{((MyToken)tok).Text}\"");
-            }
+                Console.WriteLine($"{tok.Class ?? "ERROR",-20} = {tok}");
+            }*/
+            SList sres = new SList(ParseSExpr(output.Select(t => t as MyToken)));
+            Console.WriteLine(sres);
             Console.ReadLine();
         }
     }
---
+++ SParser/SExpr.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SParser
+{
+    enum SType { List, Atom, Srting }
+    abstract class SExpr
+    {
+        public SType Type { get; protected set; }
+        public abstract string ToString(int deep);
+        protected SExpr(SType type)
+        {
+            Type = type;
+        }
+    }
+    class SAtom : SExpr
+    {
+        public string Name;
+        public SAtom(string name) : this(name, SType.Atom) { }
+        protected SAtom(string name, SType type) : base(type)
+        {
+            Name = name;
+        }
+        public override string ToString(int deep) => new string(' ', deep * 4) + Name;
+        public override string ToString() => Name;
+    }
+    class SString : SAtom
+    {
+        public SString(string name) : base(name, SType.Srting) { }
+    }
+    class SList : SExpr, IEnumerable<SExpr>
+    {
+        public List<SExpr> Children;
+
+        public SList() : base(SType.Atom)
+        {
+            Children = new List<SExpr>();
+        }
+        public SList(IEnumerable<SExpr> collection) : base(SType.Atom)
+        {
+            Children = new List<SExpr>(collection);
+        }
+
+        public void Add(SExpr item)
+        {
+            Children.Add(item);
+        }
+
+        public override string ToString(int deep)
+        {
+            StringBuilder res = new StringBuilder();
+            res.Append(new string(' ', deep * 4));
+            res.Append("(");
+            if (Children.Count > 0)
+                res.AppendLine();
+            foreach (var item in Children)
+                res.AppendLine(item.ToString(deep + 1));
+            if (Children.Count > 0)
+                res.Append(new string(' ', deep * 4));
+            res.Append(")");
+            return res.ToString();
+        }
+
+        public override string ToString() => ToString(0);
+
+        public IEnumerator<SExpr> GetEnumerator()
+        {
+            return ((IEnumerable<SExpr>)Children).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<SExpr>)Children).GetEnumerator();
+        }
+    }
+}
